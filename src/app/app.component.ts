fb2c53bd8879dbaa610d0efb6d7043e1e4603a49
Cristian Quispe <cquispe@vectoritcgroup.com>
2017-08-11 19:21:25 -0500

    added some iteractions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,11 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
   acountStateList;
+  acountStateFilteredList;
+  onChangeYear (year) {
+    console.log(year);
+    this.acountStateFilteredList = this.acountStateList.filter(item => item.year == year);
+  }
   ngOnInit(): void {
     this.acountStateList = [
       {year: 2017, month: {shorName: 'ENE'}, state: 1},
@@ -21,6 +26,19 @@ export class AppComponent implements OnInit {
       {year: 2017, month: {shorName: 'OCT'}, state: 3},
       {year: 2017, month: {shorName: 'NOV'}, state: 3},
       {year: 2017, month: {shorName: 'DIC'}, state: 3},
+      {year: 2016, month: {shorName: 'ENE'}, state: 2},
+      {year: 2016, month: {shorName: 'FEB'}, state: 2},
+      {year: 2016, month: {shorName: 'MAR'}, state: 1},
+      {year: 2016, month: {shorName: 'ABR'}, state: 1},
+      {year: 2016, month: {shorName: 'MAY'}, state: 1},
+      {year: 2016, month: {shorName: 'JUN'}, state: 2},
+      {year: 2016, month: {shorName: 'JUL'}, state: 2},
+      {year: 2016, month: {shorName: 'AGO'}, state: 3},
+      {year: 2016, month: {shorName: 'SET'}, state: 1},
+      {year: 2016, month: {shorName: 'OCT'}, state: 1},
+      {year: 2016, month: {shorName: 'NOV'}, state: 3},
+      {year: 2016, month: {shorName: 'DIC'}, state: 3},
     ];
+    this.onChangeYear(2017);
   }
 }
